1a1ac9b1a6b6210a4aed0fff77d501c00ca238dc
LexuanLiu123 <lel006@ucsd.edu>
2021-05-03 19:40:01 -0700

    plotting two graphs

--- src/App.js
+++ src/App.js
@@ -5,9 +5,6 @@ import Chart from './components/Chart';
 function App() {
   return (
     <div className="App">
-      <div className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </div>
       <Chart />
     </div>
   );
--- src/components/Chart.js
+++ src/components/Chart.js
@@ -1,39 +1,28 @@
 import React, { Component } from 'react';
 import { Scatter } from 'react-chartjs-2';
-import dataset from './single_scatter_data.json';
+import scatter_dataset from './scatter_data.json';
+
+const first_chart = {
+    datasets: [
+        {
+            backgroundColor: 'blue',
+            data: scatter_dataset['chart_1']
+        },
+        {
+            backgroundColor: 'red',
+            data: scatter_dataset['chart_2']
+        },
+    ],
+};
 
 class Chart extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            datasets: [
-                {
-                    label: 'My First dataset',
-                    fill: false,
-                    backgroundColor: 'rgba(75,192,192,0.4)',
-                    pointBorderColor: 'rgba(75,192,192,1)',
-                    pointBackgroundColor: '#fff',
-                    pointBorderWidth: 3,
-                    pointHoverRadius: 8,
-                    pointHoverBackgroundColor: 'rgba(75,192,192,1)',
-                    pointHoverBorderColor: 'rgba(220,220,220,1)',
-                    pointHoverBorderWidth: 3,
-                    pointRadius: 2,
-                    pointHitRadius: 10,
-                    data: dataset
-                },
-            ],
-        };
-    }
 
 
     render() {
 
         return (
             <div className="chart">
-                <Scatter
-                    data={this.state}
-                />
+                <Scatter data={first_chart} />
             </div>
         )
     }
